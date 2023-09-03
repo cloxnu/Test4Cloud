a65b92b48e8c862b25518e8b453b52fc3bad9fe8
Sidney Liu <cloxnu@outlook.com>
2023-09-03 14:56:43 +0800

    init

---
+++ Test4Cloud/Test4Cloud/Test4CloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test4CloudApp.swift
+//  Test4Cloud
+//
+//  Created by Sidney Liu on 2023/9/3.
+//
+
+import SwiftUI
+
+@main
+struct Test4CloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
